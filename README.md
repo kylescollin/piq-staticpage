a2433418f608b18bb791b1225dbcf58a4cc74f86
Kyle Scollin <kscollin@gmail.com>
2015-02-25 12:44:20 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+PlaceIQ Static Page
+=====================
+
+DESCRIPTION
+-----------
+This is a simple static page mobile website, ideally served as a mobile ad. It is built to be retina ready utilizing double sized assets that should responsively fit for small screens as well. This unit is just a simple static page with tappable areas.
+
+
+USER FLOW
+---------
+
+
+HOW TO UPDATE
+-------------
+1. Replace all of the necessary graphic files (bg.png). If the file names have changed, update them in index.html.
+2. Update the top of styles.less - change the header and footer height variables to reflect the size of the graphics, change the color of the location choice buttons, and update the location details colors.
+3. Make sure to compile the .less file into a .css file
+4. Update the Google Analytics UA code in the js/ga.js file.
---
+++ js/ga.js
@@ -0,0 +1,36 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', '############', 'placeiq.com');
+ga('send', 'pageview');
+
+ga('send', 'event', 'In-Page Events', 'Events', 'Page Visit');
+
+var firstinteraction = 0;
+
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'Exit Links', 'Click', url, {'hitCallback':
+     function () {
+     	if(firstinteraction === 0){
+     		firstinteraction = 1;
+     		ga('send', 'event', 'In-Page Events', 'Events', 'First Interaction', {'hitCallback':
+				function(){document.location = url}
+     		});
+            console.log("First Interaction");
+     	} else {
+     		document.location = url;
+     	}
+     }
+   });
+}
+
+function trackEvent(el){
+	if(firstinteraction === 0){
+     	firstinteraction = 1;
+     	ga('send', 'event', 'In-Page Events', 'Events', 'First Interaction');
+        console.log("First Interaction");
+    }
+	ga('send', 'event', el.getAttribute("ga-category"), el.getAttribute("ga-action"), el.getAttribute("ga-label"));
+}
